67055530e0cef3b379943e5be9fbbe69da02086a
Jdm1998 <42845727+Jdm1998@users.noreply.github.com>
2023-07-29 20:15:52 +0530

    basic structure and implemention of todo list

---
+++ script.js
@@ -0,0 +1,39 @@
+let addButton = document.getElementById("inputadd");
+let input = document.getElementById("inputtask");
+
+let tasksElement = document.getElementById("tasks");
+
+let taskinput;
+let taskList=[];
+addButton.addEventListener("click",function (params) {
+
+    taskinput= input.value.trim();
+    if(taskinput==""){
+        alert("enter something")
+    }
+
+    input.value ="";
+console.log(taskinput);
+
+let newtask = document.createTextNode( `${taskinput}`);
+let newtaskspan = document.createElement("span");
+let newtaskdiv =document.createElement("div");
+newtaskdiv.classList.add("task");
+let delelement = document.createElement('img');
+
+delelement.setAttribute("src","delete.png");
+newtaskspan.appendChild(newtask);
+newtaskdiv.appendChild(newtaskspan);
+newtaskdiv.appendChild(delelement);
+tasksElement.appendChild(newtaskdiv);
+
+})
+
+tasksElement.addEventListener("click",function(event){
+    console.log(event.target.localName);
+    console.log(event);
+    if(event.target.localName=="img"){
+        console.log("clicked on del");
+        console.log(event.target.parentElement.remove());
+    }
+})
